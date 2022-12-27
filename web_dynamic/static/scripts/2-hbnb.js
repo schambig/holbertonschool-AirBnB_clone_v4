de84854c807099c2150bd308bae43eeffb26103c
Salomon Chambi <salomonchambi@gmail.com>
2022-12-27 14:02:59 -0500

    feat: request from a given route, add or remove `available` class

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(function () {
+  const amenitiesChecked = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenitiesChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenitiesChecked[$(this).data('id')];
+    }
+    const objs = Object.values(amenitiesChecked);
+    console.log(Object.values(amenitiesChecked));
+    if (objs) {
+      $('.amenities > h4').text(Object.values(amenitiesChecked).join(', '));
+    } else {
+      $('.amenities > h4').html('&nbsp;');
+    }
+  });
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
