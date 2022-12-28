4ac6dee1e6814f107afe432c44168c2b08c4504f
Salomon Chambi <salomonchambi@gmail.com>
2022-12-28 18:30:29 -0500

    feat: request from `${window.location.hostname}` hostname

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,7 +14,7 @@ $(function () {
       $('.amenities > h4').html('&nbsp;');
     }
   });
-  $.getJSON('http://127.0.0.1:5001/api/v1/status/', (data) => {
+  $.getJSON('http://${window.location.hostname}:5001/api/v1/status/', (data) => {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
